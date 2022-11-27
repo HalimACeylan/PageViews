190e024311302c6745ceadb9564116d430d0dcd2
U190709071 <gulsenbeyzayilmazer@posta.mu.edu.tr>
2022-11-27 23:39:19 +0300

    pages are changed

--- app/src/main/java/gui/ceng/mu/edu/pageviews/MainActivity.java
+++ app/src/main/java/gui/ceng/mu/edu/pageviews/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.type_of_material_list);
+        setContentView(R.layout.takephoto);
     }
 }
