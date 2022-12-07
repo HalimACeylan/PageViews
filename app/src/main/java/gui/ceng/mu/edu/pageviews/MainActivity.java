dc111d69c9d6d79b544dd05adca52c7d95d3de46
U190709071 <gulsenbeyzayilmazer@posta.mu.edu.tr>
2022-12-08 02:07:35 +0300

    pages are disagned

--- app/src/main/java/gui/ceng/mu/edu/pageviews/MainActivity.java
+++ app/src/main/java/gui/ceng/mu/edu/pageviews/MainActivity.java
@@ -2,6 +2,7 @@ package gui.ceng.mu.edu.pageviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.takephoto);
+        setContentView(R.layout.login);
+
     }
+
 }
